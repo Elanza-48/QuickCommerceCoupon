740fd6bd13c93b4edb5610b5f7a99d3929f6beee
Elanza-48 <rishikundu6@gmail.com>
2024-09-02 21:14:45 +0530

    Initial Commit.

---
+++ src/test/java/com/elanza48/quickcard_coupon/QuickcartCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elanza48.quickcart_coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickcartCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
